e72e87831be75cb1edcb93fa8d5f1739550063ef
jins-jinx <itsallaboutjins@gmail.com>
2024-12-01 01:43:45 +0800

    Added some styles for the search text from

--- homepage.php
+++ homepage.php
@@ -12,7 +12,10 @@ include("connect.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="dashboard.css">
+    <link rel="stylesheet" href="search.css">
     <title>Homepage</title>
+
+    
 </head>
 <body>
     <div class="container" id=dashboard>
